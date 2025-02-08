24f15bc4f232d5873f4f05ecf2d4fc57f764ceac
Jack Atkinson <2115953a@student.gla.ac.uk>
2025-02-08 18:40:34 +0000

    Chapter 3 Commit 3

--- rango/views.py
+++ rango/views.py
@@ -3,9 +3,9 @@ from django.http import HttpResponse
 from django.urls import reverse
 
 def index(request):
-    about_url = reverse('about')
+    about_url = reverse('rango:about')
     return HttpResponse(f"Rango says hey there partner! <a href='{about_url}'>About</a>")
 
 def about(request):
-    index_url = reverse('index')
+    index_url = reverse('rango:index')
     return HttpResponse(f"Rango says here is the about page. <a href='{index_url}'>Index</a>")
